adc427105c8f13252bee5875c0df674ccbd8259e
Sashank Parajuli <shanks@Sashanks-Mac-mini.local>
2026-06-03 21:58:13 +0545

    feat : add basic ui for login page

--- setup-windows.ps1
+++ setup-windows.ps1
@@ -1,4 +1,4 @@
-Write-Host "Installing Qt, CMake, Ninja..." -ForegroundColor Green
+'Write-Host "Installing Qt, CMake, Ninja..." -ForegroundColor Green
 
 # Install winget packages
 winget install -e --id Kitware.CMake
@@ -8,3 +8,4 @@ winget install -e --id Qt.QtCreator
 Write-Host "Done! Now run:" -ForegroundColor Green
 Write-Host "cmake -B build" -ForegroundColor Yellow
 Write-Host "cmake --build build" -ForegroundColor Yellow
+'
